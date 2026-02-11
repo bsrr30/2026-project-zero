3db0a953e03eb236ad2576b91a6b7383262dd007
bsrr30 <reddyb3165@masseyhigh.school.nz>
2026-02-12 12:05:56 +1300

    Add files via upload

---
+++ c_01_make_statement.py
@@ -0,0 +1,9 @@
+# The functions go here
+def make_statement(statement, decoration):
+
+    print(f"{decoration * 3}: {statement} {decoration * 3}")
+
+
+#main code here
+
+make_statement("programing is fun", "👌")
